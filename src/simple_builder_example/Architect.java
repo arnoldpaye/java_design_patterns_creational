71030ab2c7ea23280f9986e875dfc14a283c38f8
Arnold Paye <arnoldpayet@gmail.com>
2022-07-02 10:08:42 -0400

    refactor: replace constructor with builder

--- src/simple_builder_example/Architect.java
+++ src/simple_builder_example/Architect.java
@@ -9,7 +9,7 @@ import java.awt.Dimension;
  */
 public class Architect {
   public static void main(String[] args) {
-    Bedroom room = new Bedroom(new Dimension(200, 100), 132, 2, Color.yellow, 2, 1, true, true);
+    Bedroom room = new BedroomBuilder().setDimensions(new Dimension(200, 100)).setCeilingHeight(132).setFloorNumber(2).setWallColor(Color.yellow).setNumberOfWindows(2).setNumberOfDoors(1).setIsDouble(true).setHasEnsuite(true).createBedroom();
   }
 }
 
