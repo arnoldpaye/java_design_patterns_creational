6ad6e6ece4fa08ea65fdaa3538e7c9c9c975cc5f
Arnold Paye <arnoldpayet@gmail.com>
2022-08-07 14:08:23 -0400

    implement abstract factory

--- src/abstract_factory_example/BikeBuilder.java
+++ src/abstract_factory_example/BikeBuilder.java
@@ -11,7 +11,7 @@ public class BikeBuilder {
         
         createBikeWithoutAbstractFactory();              
         
-        createBikeWithAbstractFactory("mountain bike");
+        createBikeWithAbstractFactory("road bike");
         
     }
     
