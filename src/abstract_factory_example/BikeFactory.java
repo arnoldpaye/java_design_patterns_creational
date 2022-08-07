315f006fb67f12d62dbb77add4267b0ebd6ce427
Arnold Paye <arnoldpayet@gmail.com>
2022-08-07 13:55:55 -0400

    add abstract_factory_example package

---
+++ src/abstract_factory_example/BikeFactory.java
@@ -0,0 +1,14 @@
+
+package abstract_factory_example;
+
+/**
+ *
+ * @author arnoldpaye
+ */
+public abstract class BikeFactory {        
+    
+    abstract Tire createTire();
+    
+    abstract Handlebar createHandlebar();        
+    
+}
---
+++ src/abstract_factory_example/Handlebar.java
@@ -0,0 +1,12 @@
+
+package abstract_factory_example;
+
+/**
+ *
+ * @author arnoldpaye
+ */
+public abstract class Handlebar {
+    
+    abstract void getDescription();
+    
+}
